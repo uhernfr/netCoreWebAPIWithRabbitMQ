6f26736c3068f8bc909e097ba1cc526e2e5ac756
uhernfr <fhc.silv@gmail.com>
2020-08-11 16:09:49 -0300

    Initial Commit

---
+++ Controllers/RabbitMQConsumerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace netCoreWebAPIWithRabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMQConsumerController : ControllerBase
+    {
+        // GET: api/RabbitMQConsumer
+        [HttpGet]
+        public string Get()
+        {
+            var factoryCons = new ConnectionFactory() { HostName = "localhost" };
+            var connectionCons = factoryCons.CreateConnection();
+            var channelCons = connectionCons.CreateModel();
+            
+                channelCons.QueueDeclare(queue: "nomeFila",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channelCons);
+                var message = string.Empty;
+
+            channelCons.BasicConsume(queue: "nomeFila",
+                                autoAck: false,
+                                consumer: consumer);
+           
+            consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        message = Encoding.UTF8.GetString(body);
+                        
+                        channelCons.BasicAck(ea.DeliveryTag, false);
+                       
+                    }catch(Exception ex)
+                    {
+                        channelCons.BasicNack(ea.DeliveryTag, false,false);
+                    }
+
+                    consumer.Shutdown += (o, a) => Console.WriteLine("Consumer Shutdown");
+                 
+                };
+
+            Thread.Sleep(3000);
+            return message;
+        }       
+       
+    }
+}
---
+++ Controllers/RabbitMQProducerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace netCoreWebAPIWithRabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMQProducerController : ControllerBase
+    {
+        // POST: api/RabbitMQProducer
+        [HttpPost]
+        public string Post(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+           
+                channel.QueueDeclare(queue: "nomeFila",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "nomeFila",
+                                     basicProperties: null,
+                                     body: body);
+
+            return "Mensagem enviada para a fila";
+        }
+
+       
+    }
+}
